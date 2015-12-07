da49a50c074dab53293054dca5c709ddec2a2932
Jared Smith <jared@jaredwsmith.com>
2015-12-06 22:31:07 -0500

    Clean up duplicate logging

--- product-ingest.php
+++ product-ingest.php
@@ -6,6 +6,11 @@
  * Main entry point for LDM ingest. This hands off to a factory which generates a class for specific products.
  * Many thanks to @blairblends, @edarc, and the Updraft team for help and inspiration
  */
+
+//
+// Execution time
+//
+
 $time_start = microtime(true);
 
 //
@@ -28,13 +33,13 @@ include('inc/PubSub.php');
 // 
 $relay = new Dispatcher();
 
-// Include console listener in debug mode
-if(DEBUG_MODE) {
-	include('inc/endpoints/ConsoleListener.class.php');
-}
-
 // Logging listener
 include('inc/endpoints/LogListener.class.php');
+
+// Include console listener in debug mode
+if(defined('DEBUG_MODE') && DEBUG_MODE) {
+	include('inc/endpoints/ConsoleListener.class.php');
+}
 
 // Bring in the abstract class definition for NWSProduct.
 include('inc/NWSProduct.class.php');
@@ -44,26 +49,6 @@ include('inc/NWSProductFactory.class.php');
 
 // Geodata library
 include('inc/geo/GeoLookup.class.php');
-
-//
-// Set up logging
-// 
-
-$log_endpoint = new LogListener();
-if(defined('DEBUG_MODE') && DEBUG_MODE)
-{
-	$log_level = "*";
-}
-else
-{
-	$log_level = "ERR";
-}
-$relay->subscribe($log_endpoint,'log',$log_level);
-
-
-//
-// Execution time
-//
 
 // Get the file path from the command line.
 // TODO: Consider piping this in, may save a small bit of disk I/O

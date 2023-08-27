2520337e22cc18defd42406e4713abac4873aa0a
Jared Smith <jared@jaredwsmith.com>
2023-08-27 15:52:44 -0400

    Initial TCM and TCP files

---
+++ src/Parser/ProductTypes/TCM.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * TCM parser for NHC advisories
+ */
+
+namespace chswx\LDMIngest\Parser\ProductTypes;
+
+use chswx\LDMIngest\Parser\NWSProduct;
+
+class TCM extends NWSProduct
+{
+}
---
+++ src/Parser/ProductTypes/TCP.php
@@ -0,0 +1,8 @@
+<?php
+
+// SUMMARY OF (.*)\.\.\.(\d+)+ UTC\.\.\.INFORMATION
+// LOCATION...(.*) (.*)
+// ABOUT (.*) MI\.\.\.(.*) KM (.*)
+// MAXIMUM SUSTAINED WINDS\.\.\.(.*) MPH\.\.\.(.*) KM\/H
+// PRESENT MOVEMENT\.\.\.(.*)
+// MINIMUM CENTRAL PRESSURE\.\.\.(.*) MB\.\.\.(.*) INCHES

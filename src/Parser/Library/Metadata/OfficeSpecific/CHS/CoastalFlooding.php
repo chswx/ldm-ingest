f19a4b61e2ca41afd909657d15a9ffd811982cbf
Jared Smith <jared@jaredwsmith.com>
2024-11-17 22:12:52 -0500

    Water level forecast range first stab

--- src/Parser/Library/Metadata/OfficeSpecific/CHS/CoastalFlooding.php
+++ src/Parser/Library/Metadata/OfficeSpecific/CHS/CoastalFlooding.php
@@ -8,15 +8,23 @@ class CoastalFlooding
 {
     public static function totalWaterLevelForecastRange($segment_text): array
     {
-        /* (7.8 to 8.0 feet Mean Lower Low Water at Charleston) */
+        $result = [];
 
-        $pattern = "";
+        // Example: (7.8 to 8.0 feet Mean Lower Low Water at Charleston)
+        $pattern = '/\((\d\.\d) to (\d\.\d) feet Mean Lower Low Water at (\w+)\)]/';
 
         // Grab the bit of text that will have the forecast range
         $text = Utils::findBulletPoint($segment_text, "WHAT");
         if (preg_match($pattern, $text, $matches)) {
+            $result = [
+                'twl_min'        => $matches[1],
+                'twl_max'        => $matches[2],
+                'twl_fcst_point' => $matches[3]
+            ];
+        } else {
+            Utils::debug("No match found within $text");
         }
 
-        return [];
+        return $result;
     }
 }
